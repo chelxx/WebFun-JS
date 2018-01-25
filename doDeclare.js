f32561ba5e5e331c887fcc5228e28f8d04a59629
Chelsea Cabalan <chex239@gmail.com>
2018-01-25 16:00:06 -0600

    Javascript Assignments

---
+++ doDeclare.js
@@ -0,0 +1,27 @@
+// I DO DECLARE! Assignment
+
+var boo1 = 27;
+var boo2 = "apples";
+var num1 = 143;
+var num2 = 9000;
+var num3 = 1000000;
+var num4 = -10.50;
+var string1 = "Chelsea!";
+var string2 = "I'm good. Thanks for asking!";
+var string3 = "I stink!";
+
+if (boo1 == 27) { //Boolean #1
+    console.log ("True! That's my age!");
+}
+if (boo2 == "oranges") { //Boolean #2
+    console.log ("Yes, I want to eat that!")
+} else {
+    console.log ("False! I don't want to eat that!")
+}
+console.log ("I love you:", num1); //Number #1
+console.log ("It's over...", num2); //Number #2
+console.log ("Write a million in numbers, please.", num3); //Number #3
+console.log ("Negative number:", num4); //Number #4
+console.log ("What's your name?", string1); //String #1
+console.log ("How are you today?", string2); //String #2
+console.log ("Are you good at algorithms?", string3); //String #3
