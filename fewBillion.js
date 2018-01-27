7dd2e5c842bc56ac99569f0ed6ab427f6f62bb5e
Chelsea Cabalan <chex239@gmail.com>
2018-01-27 15:30:44 -0600

    Javascript Assignments

---
+++ fewBillion.js
@@ -0,0 +1,9 @@
+//For a Few Billion Assignment
+
+var servant = 0.01;
+
+for (var i = 1; i < 31; i++) {
+    servant = servant * 2;
+    console.log (servant);
+}
+console.log ("The servant received this much money: $" + servant);
