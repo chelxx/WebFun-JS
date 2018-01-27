ba05b5f3c840a6c5f106096b247f6f224988577f
Chelsea Cabalan <chex239@gmail.com>
2018-01-27 15:43:03 -0600

    Javascript Assignments

--- fewBillion.js
+++ fewBillion.js
@@ -3,7 +3,7 @@
 var servant = 0.01;
 
 for (var i = 1; i < 31; i++) {
-    servant = servant * 2;
     console.log ("Day " + i + ": $" +servant);
+    servant = servant * 2;
 }
 console.log ("The servant received a total of $" + servant + " in 30 days.");
