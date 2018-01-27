79f05de0f12c0e6ef9afc55e8db5c8fc5dc5bf96
Chelsea Cabalan <chex239@gmail.com>
2018-01-27 14:44:15 -0600

    Javascript Assignments

--- haveTime.js
+++ haveTime.js
@@ -7,8 +7,18 @@ var PERIOD = "AM";
 // var MINUTE = 15;
 // var PERIOD = "PM";
 
-if (HOUR == 8 && MINUTE == 50 && PERIOD == "AM") {
-    console.log ("It's almost 9 in the morning!");
-} else if (HOUR == 7 && MINUTE == 15 && PERIOD == "PM"){
-    console.log ("It's just after 7 in the evening!")
+// if (HOUR == 8 && MINUTE == 50 && PERIOD == "AM") {
+//     console.log ("It's almost 9 in the morning!");
+// } else if (HOUR == 7 && MINUTE == 15 && PERIOD == "PM"){
+//     console.log ("It's just after 7 in the evening!")
+// }
+
+if (MINUTE < 30 && PERIOD == "AM") {
+    console.log ("It's just after " + HOUR + " in the morning!");
+} else if (MINUTE > 30 && PERIOD == "AM") {
+    console.log ("It's almost " + (HOUR+1) + " in the morning!");
+} else if (MINUTE > 30 && PERIOD == "PM") {
+    console.log ("It's almost " + (HOUR+1) + " in the evening!");
+} else if (MINUTE < 30 && PERIOD == "PM") {
+    console.log ("It's just after " + HOUR + " in the evening!");
 }
