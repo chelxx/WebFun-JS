f53da4b02313f8041da10f8eecb67cc620d7b0a6
Chelsea Cabalan <chex239@gmail.com>
2018-01-27 15:11:52 -0600

    Javascript Assignments

--- whileWait.js
+++ whileWait.js
@@ -4,12 +4,12 @@ var daysBday = 60;
 
 for (var daysBday = 60; daysBday > -1; daysBday--) {
     if (daysBday > 30) {
-        console.log ("I'm so sad! My birthday is too far away! I have to wait " + daysBday + " more!");
+        console.log ("I'm so sad! My birthday is too far away! I have to wait " + daysBday + " days more!");
     }
     if (daysBday < 30) {
-        console.log ("I'm getting excited! My birthday is getting closer! I have to wait " + daysBday + " more!");
+        console.log ("I'm getting excited! My birthday is getting closer! I have to wait " + daysBday + " days more!");
     }
     if (daysBday == 0) {
-        console.log ("Yay! Happy Birthday!");
+        console.log ("Yay~ Today is the day! Happy Birthday!");
     }
 }
