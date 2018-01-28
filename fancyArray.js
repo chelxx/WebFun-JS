a50a6b8f6f68605f04bd9a5677c306456002cfcb
Chelsea Cabalan <chex239@gmail.com>
2018-01-28 13:02:21 -0600

    Javascript Assignments

---
+++ fancyArray.js
@@ -0,0 +1,8 @@
+//Fancy Array Assignment
+
+function fancyArray (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log (i + "->" + arr[i]);
+    }
+}
+fancyArray (["James", "Jill", "Jane", "Jack"]);
