5e79f9ac2e389fea9a3a38facb257d3928d43ba0
Chelsea Cabalan <chex239@gmail.com>
2018-01-28 17:06:41 -0600

    Javascript Message

--- rangePrint.js
+++ rangePrint.js
@@ -1,10 +1,9 @@
 //Range Print Assignment
 
 function rangePrint (arr) {
-    var arr = [2,10,2];
 
     for (var i = arr[0]; i < arr[1]; i = i + arr[2]) {
         console.log (i);
     }
 }
-rangePrint ();
+rangePrint ([2,10,2]);
