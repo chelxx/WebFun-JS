5f598e78b2890f7d11b154e8e72fafb62ca39b24
Chelsea Cabalan <chex239@gmail.com>
2018-01-29 17:09:16 -0600

    Javascript Assignments

--- studentsInstruct.js
+++ studentsInstruct.js
@@ -13,3 +13,24 @@ function callNames () {
     }
 }
 callNames ();
+
+//Part II
+// function groupies () {
+//     var users = {
+//         'Students': [ 
+//             {first_name:  'Michael', last_name : 'Jordan'},
+//             {first_name : 'John', last_name : 'Rosales'},
+//             {first_name : 'Mark', last_name : 'Guillen'},
+//             {first_name : 'KB', last_name : 'Tonel'}
+//          ],
+//         'Instructors': [
+//             {first_name : 'Michael', last_name : 'Choi'},
+//             {first_name : 'Martin', last_name : 'Puryear'}
+//          ]
+//         }
+
+//     for (var i = 0; i < users['Students'].length; i++){
+//         console.log ((i+1) + " - " + users['Students'][i].first_name + " " + users['Students'][i].last_name + " - ");
+//     }
+// }
+// groupies ();
