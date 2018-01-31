cfaa6c4eb2212b7f32e9133d139c0466b9dc434d
Chelsea Cabalan <chex239@gmail.com>
2018-01-30 18:38:57 -0600

    Javascript Assignments

---
+++ jQuery/Pokemon/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){ //So nostalgic...
+    for (var i = 1; i < 152; i++){
+        $('#pokemon').append("<img src='http://pokeapi.co/media/img/" + i + ".png'>")
+    }
+});
--- jQuery/jQueryFxns/script.js
+++ jQuery/jQueryFxns/script.js
@@ -1,20 +1,4 @@
 $(document).ready(function(){
-    $(".clickme").click(function(){
-        alert("You just clicked me!")
-    });
 
-    $(".buttontog").click(function(){
-        $(".toggleme").toggle(function(){});
-    });
-
-    $(".prepend").click(function() {
-        $('.appendme').prepend("<b> THIS IS THE PREPEND! <b>");
-    });
-
-    $(".append").click(function() {
-        $('.appendme').append("<b> ...AND THIS IS THE APPEND! <b>");
-    });
     
 });
-
-$("").___(function(){});
