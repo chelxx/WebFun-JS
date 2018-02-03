c98c259ee4d40dfe1407cc4c4d8b39cd05a1241a
Chelsea Cabalan <chex239@gmail.com>
2018-02-02 20:12:43 -0600

    Javascript Assignments

--- jQuery/Weather/script.js
+++ jQuery/Weather/script.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
     $('button').click('submit', function() {
-        // your code here (build up your url)
         var loc = $('form').find('input[name="location"]').val();
-
         $.get('http://api.openweathermap.org/data/2.5/weather?q='+loc+'&APPID=f807c5ca4818d9e403ea91a1856a709f', function(res) {
-            // your code here
             var html_str = "";
             var temp = Math.trunc(res.main.temp*9/5-459.67);
             html_str += "<h1>" + res.name + "</h1>";
@@ -12,7 +9,6 @@ $(document).ready(function() {
             $('div#input').html(html_str);
             console.log (res);
         }, 'json');
-        // don't forget to return false so the page doesn't refresh
         return false;
     });
 });
